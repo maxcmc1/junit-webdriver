362a5cdcc071bd7d62f33238c321ecc128573aee
Slava Skryabin <vs8199@gmail.com>
2020-05-01 22:01:52 -0700

    Merge master into ticket1

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -1,5 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
